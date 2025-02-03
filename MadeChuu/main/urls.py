ba60248bdfc3b982f65ca3193db255ff3ca46814
Weds07 <zmrdiwkung@gmail.com>
2025-02-03 13:59:33 +0700

    chat
    i make a chat.html and path for chat in urls.py

--- MadeChuu/main/urls.py
+++ MadeChuu/main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from main import views
+from .views import chat
+
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('chat/', chat, name='chat')
 ]
