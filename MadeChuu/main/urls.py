973d1be9ebaf28fb26f2d6d452175c221a764b85
kittipongrks <icafez4444@gmail.com>
2025-02-04 16:15:08 +0700

    again and again

--- MadeChuu/main/urls.py
+++ MadeChuu/main/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from main import views
 urlpatterns = [
     path('',views.index),
-    path('chatAdmin',views.ChatAdmin),
-    path('OrderCheck',views.OrderAdmin)
+    path('ChatsAdmin/',views.ChatAdmin , name = 'ChatsAdmin'),
+    path('OrderAdmin/',views.OrderAdmin, name = 'OrderAdmin'),
+    path('Dashboard/',views.Dashboard, name = 'Dashboard'),
+    path('CommentsAdmin/',views.CommentAdmin, name = 'CommentsAdmin'),
+    path('ProductsAdmin/',views.ProductAdmin, name = 'ProductsAdmin'),
+    path('PromotionsAdmin/',views.PromotionsAdmin, name = 'PromotionsAdmin')
+
 ]
--- MadeChuu/main/views.py
+++ MadeChuu/main/views.py
@@ -4,9 +4,21 @@ def index(request):
     return render(request, 'index.html')
 
 def ChatAdmin(request):
-    return render(request , "ChatAdmin.html")
+    return render(request , "ChatsAdmin.html")
 
 def OrderAdmin(request):
     #Query from model Order
     data = Order.objects.all()
-    return render(request , "OrderCheckAdmin.html" , {'Orders':data})
+    return render(request , "OrderAdmin.html" , {'Orders':data})
+
+def Dashboard(request):
+    return render(request , "Dashboard.html")
+
+def ProductAdmin(request):
+    return render(request , "ProductsAdmin.html")
+
+def PromotionsAdmin(request):
+    return render(request , "PromotionsAdmin.html")
+
+def CommentAdmin(request):
+    return render(request , "CommentAdmin.html")
