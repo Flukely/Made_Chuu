9336c7e03128bcb226bcdf04e16b1b08cddce9bd
kittipongrks <icafez4444@gmail.com>
2025-02-04 09:32:34 +0700

    add moremorefile

--- MadeChuu/main/views.py
+++ MadeChuu/main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-
+from .models import Order , OrderProducts
 def index(request):
     return render(request, 'index.html')
+
+def ChatAdmin(request):
+    return render(request , "ChatAdmin.html")
+
+def OrderAdmin(request):
+    #Query from model Order
+    data = Order.objects.all()
+    return render(request , "OrderAdmin.html" , {'orders':data})
