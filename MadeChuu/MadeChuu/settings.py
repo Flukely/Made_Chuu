03c59404708912cdca047ab4060255cf4fd62167
NekoSama01 <hamza555444552@gmail.com>
2025-02-05 22:38:56 +0700

    1

--- MadeChuu/MadeChuu/settings.py
+++ MadeChuu/MadeChuu/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',  # เพิ่ม app ที่ชื่อ main เข้าไป
+    'main',# เพิ่ม app ที่ชื่อ main เข้าไป
+    'ProductDetail',# เพิ่ม app ที่ชื่อ ProductDetail เข้าไป
 ]
 
 MIDDLEWARE = [
