ff3cfdff8f4596fc02d1fc778df28e00f7b1fa1c
kittipongrks <icafez4444@gmail.com>
2025-02-06 21:05:35 +0700

    change a little ver2

--- MadeChuu/admin_function/models.py
+++ MadeChuu/admin_function/models.py
@@ -128,7 +128,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     quantity = models.IntegerField(blank=True, null=True)
     category = models.ForeignKey(Category, models.DO_NOTHING, blank=True, null=True)
-    product_image = models.ImageField(upload_to='MadeChuu/images/')
+    product_image = models.ImageField(upload_to='images/')
     created = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.category_name
