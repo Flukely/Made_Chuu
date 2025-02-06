23f285c7fe7fa5092f970beb1be5c6914737a7be
kittipongrks <icafez4444@gmail.com>
2025-02-06 10:45:54 +0700

    before everything change

--- MadeChuu/admin_function/views.py
+++ MadeChuu/admin_function/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Order
 
 def admin_function(request):
     return render(request, 'admin_function/Dashboard.html')
@@ -7,7 +8,9 @@ def ChatAdmin(request):
     return render(request, 'admin_function/ChatsAdmin.html')
 
 def OrderAdmin(request):
-    return render(request, 'admin_function/OrderAdmin.html')
+    #Query from model Order
+    Order_wait_status = Order.objects.filter(status_order = "wait")
+    return render(request, 'admin_function/OrderAdmin.html',{'Orders_wait': Order_wait_status})
 
 def Dashboard(request):
     return render(request, 'admin_function/Dashboard.html')
