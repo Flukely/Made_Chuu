b6e558b61bd7f46e14bbc3344ce9791eaa649ed4
NekoSama01 <hamza555444552@gmail.com>
2025-02-07 21:55:47 +0700

    ไม่ก้าวหน้า

--- MadeChuu/ProductDetail/static/js/scripts.js
+++ MadeChuu/ProductDetail/static/js/scripts.js
@@ -1,21 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-    const minusButton = document.getElementById('minus-button');
-    const plusButton = document.getElementById('plus-button');
-    const quantityInput = document.getElementById('quantity-input');
-
-    minusButton.addEventListener('click', function() {
-        let currentValue = parseInt(quantityInput.value);
-        if (currentValue > 1) {
-            quantityInput.value = currentValue - 1;
-        }
-    });
-
-    plusButton.addEventListener('click', function() {
-        let currentValue = parseInt(quantityInput.value);
-        quantityInput.value = currentValue + 1;
-    });
-});*/
-
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('.star-rating').forEach(function(element) {
         let rating = parseInt(element.getAttribute('data-rating'), 10);
