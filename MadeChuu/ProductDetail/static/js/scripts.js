044aefde750e3179205e2a96fb823df948cc00ec
NekoSama01 <hamza555444552@gmail.com>
2025-02-07 21:26:38 +0700

    ก้าวเดียว

--- MadeChuu/ProductDetail/static/js/scripts.js
+++ MadeChuu/ProductDetail/static/js/scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     const minusButton = document.getElementById('minus-button');
     const plusButton = document.getElementById('plus-button');
     const quantityInput = document.getElementById('quantity-input');
@@ -13,5 +13,13 @@ document.addEventListener('DOMContentLoaded', function() {
     plusButton.addEventListener('click', function() {
         let currentValue = parseInt(quantityInput.value);
         quantityInput.value = currentValue + 1;
+    });
+});*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.star-rating').forEach(function(element) {
+        let rating = parseInt(element.getAttribute('data-rating'), 10);
+        let stars = '★'.repeat(rating) + '☆'.repeat(5 - rating); // 5 ดาวสูงสุด
+        element.textContent = stars;
     });
 });
--- MadeChuu/ProductDetail/views.py
+++ MadeChuu/ProductDetail/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from main.models import Product, Review
+import json
 
 def product_detail(request):
     product = get_object_or_404(Product, pk=1)
-    reviews = get_object_or_404(Review, pk=1)
+    reviews = Review.objects.all()
+    reviews_avg = Review.objects.all()
     return render(request, 'product_detail.html', {"product": product,"reviews": reviews})
