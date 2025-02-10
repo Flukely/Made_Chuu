459f969ffbbcce40c871e3007a2f229b83ddeea7
NekoSama01 <hamza555444552@gmail.com>
2025-02-11 06:29:45 +0700

    ก่อนรวม

--- MadeChuu/ProductDetail/views.py
+++ MadeChuu/ProductDetail/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from main.models import Product, Review
 from django.db.models import Avg
 
-def product_detail(request):
-    product = get_object_or_404(Product, pk=1)
-#   product = get_object_or_404(Product, pk=product_id) product_id
-    reviews = Review.objects.all()
+def product_detail(request):#product_id
+    product = get_object_or_404(Product, pk=1)#pk=product_id
+#   product = get_object_or_404(Product, pk=product_id)
+    reviews = Review.objects.all()#filter(product, product_id)
     reviews_avg = reviews.aggregate(Avg('rating'))['rating__avg'] or 0
     return render(request, 'product_detail.html', {"product": product,"reviews": reviews,"average_rating": reviews_avg})
