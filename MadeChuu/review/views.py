0e6e60989b614c9bc88a20dc409510524a76bc30
SiraphopL <pleum48695@gmail.com>
2025-02-10 15:56:59 +0700

    หมดแล้วมี่ง ถ้าจะเหลือก็ user

--- MadeChuu/review/views.py
+++ MadeChuu/review/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Sum
-from .models import Product, Review, OrderProducts
+from main.models import Product, Review, OrderProduct
 from .forms import ReviewForm
 from django.http import JsonResponse
 from django.db.models import Count
@@ -10,7 +10,7 @@ def review_page(request):
     product_orders = {}
 
     for product in products:
-        orders = OrderProducts.objects.filter(product=product).exclude(
+        orders = OrderProduct.objects.filter(product=product).exclude(
             order__in=Review.objects.filter(product=product).values('order')
         )
         product_orders[product] = orders
@@ -22,7 +22,10 @@ def review_page(request):
 
 def submit_review(request, product_id, order_id):
     product = get_object_or_404(Product, pk=product_id)
-    order = get_object_or_404(OrderProducts, product=product, order__pk=order_id)
+    orders = OrderProduct.objects.filter(product=product, order__pk=order_id)
+    if orders.count() != 1:
+        return render(request, 'review/review.html', {'error': 'พบข้อผิดพลาดในการค้นหาข้อมูลการสั่งซื้อ'})
+    order = orders.first()
     if request.method == "POST":
         form = ReviewForm(request.POST, request.FILES)
         if form.is_valid():
