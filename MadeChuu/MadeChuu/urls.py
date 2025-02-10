48d91eb2cf40a4e163c105aa51578cbfc56dd4ab
Flukely <songpanonfluk@gmail.com>
2025-02-11 01:13:44 +0700

    add admin,payment,pyment,review

--- MadeChuu/MadeChuu/urls.py
+++ MadeChuu/MadeChuu/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # URL สำหรับ admin site ของ Django
     path('admin_function/', include('admin_function.urls')),  # URL สำหรับ admin_function
     path('', include('main.urls')),  # URL สำหรับแอปหลัก 
+    path('review/', include('review.urls')), # URL สำหรับ review
+    path('payment/', include('payment.urls')), # URL สำหรับ payment
+    path('receipt/', include('receipt.urls')), # URL สำหรับ receipt
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
