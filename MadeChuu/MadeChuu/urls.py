5f7ed64d3b6342cb86b5081808142d5fc30632e8
PK <145848801+Blue-160946@users.noreply.github.com>
2025-02-10 19:24:57 +0700

    Delete MadeChuu directory

--- MadeChuu/MadeChuu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # URL สำหรับ admin site ของ Django
-    path('admin_function/', include('admin_function.urls')),  # URL สำหรับ admin_function
-    path('', include('main.urls')),  # URL สำหรับแอปหลัก
-]
