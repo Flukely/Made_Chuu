635fe16ce2d768ced880a212162cbb73d8caa53a
kittipongrks <icafez4444@gmail.com>
2025-02-11 16:21:14 +0700

    newๆ a update ตามไอฟลุ๊คใน dis งับ

--- MadeChuu/product/views.py
+++ MadeChuu/product/views.py
@@ -12,9 +12,9 @@ def product(request):
     
     # Combine the filtered results
     if shop_filter.qs.exists():
-        selected_shop = shop_filter.qs.first()
-        product_filter = ProductFilter(request.GET, queryset=Product.objects.filter(shop=selected_shop))
-        categories = Category.objects.filter(product__shop=selected_shop).distinct()
+        selected_shops = shop_filter.qs
+        product_filter = ProductFilter(request.GET, queryset=Product.objects.filter(shop__in=selected_shops))
+        categories = Category.objects.filter(product__shop__in=selected_shops).distinct()
     else:
         product_filter = ProductFilter(request.GET, queryset=Product.objects.all())
     filtered_products = product_filter.qs
