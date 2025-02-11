ead0c737b20cf00fb45f58c583a8274e88ee493d
PK <145848801+Blue-160946@users.noreply.github.com>
2025-02-12 00:15:06 +0700

    Delete MadeChuu directory

--- MadeChuu/payment/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path # นำเข้าฟังก์ชัน path จาก Django
-from payment import views # 🔥 เพิ่มบรรทัดนี้เพื่อเรียกใช้ views จาก payment
-
-app_name = "payment"  # 🔥 เพิ่มบรรทัดนี้เพื่อกำหนด namespace
-
-urlpatterns = [ # กำหนด URL ของแอปพลิเคชัน payment
-    path('', views.payment, name='payment'), # 🔥 เพิ่มบรรทัดนี้เพื่อกำหนด path สำหรับหน้า payment
-]
--- MadeChuu/product/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from main.models import CartItem
-
-class CartForm(forms.ModelForm):
-    class Meta:
-        model = CartItem
-        fields = ['product', 'quantity']
