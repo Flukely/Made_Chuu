9c4e3e3a8e1f198c8191cfc2af1e93a045344e5c
Pongtham09 <Pongthamd65@nu.ac.th>
2025-02-27 14:17:17 +0700

    I

---
+++ MadeChuu/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import register, user_login, logout_view, index_view
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('', index_view, name='index'),
+]
